1d42052c2cbe16b2a0856ddd2d4f05487145bdbb
YellowMuffin <yellowmuffingigakox@gmail.com>
2023-04-03 18:28:19 +0200

    Repair the game
    naprawiłem i dodałem wszystko co potrzeba

--- ufo/Assets/Scripts/PickupAnimation.cs
+++ ufo/Assets/Scripts/PickupAnimation.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickup : MonoBehaviour
+public class PickupAnimation : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
